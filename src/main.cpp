24fce84bb99fcb51c2800b7d7e62971b54e1b40f
Rostislav Otevrel <rostislav@otevrel.net>
2024-11-11 19:12:08 +0100

    [SYNC]

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+/* #include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    lcd.init();
+    lcd.backlight();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    for (int i = 0; i < 4; i++) {
+        for (int y = 0; y < 20; y++) {
+            lcd.setCursor(y, i);
+            lcd.print("X");
+            delay(500);
+            lcd.clear();
+        }
+    }
+} */
+
+#include <Stepper.h>
+
+const int stepsPerRevolution = 200; // Change this to match your motor's steps per revolution
+const int dirPin = 2;
+const int stepPin = 3;
+
+Stepper myStepper(stepsPerRevolution, stepPin, dirPin);
+
+void setup() {
+  myStepper.setSpeed(500); // Adjust the speed as needed
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop() {
+  // Turn the motor left
+  myStepper.step(stepsPerRevolution);
+  delay(1000);
 }
