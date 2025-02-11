1ad07ea42e1e794487d21dae0d3cdbe88e1bcc27
Rostislav Otevrel <rostislav@otevrel.net>
2025-02-11 17:58:59 +0100

    [SYNC]

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,97 @@
+//* LIBRARIES: ---------------------------------------------------------------------------------
+
 #include <Arduino.h>
-/* #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+//* PINS: --------------------------------------------------------------------------------------
 
-void setup() {
-    lcd.init();
-    lcd.backlight();
+//* Sensors:
+
+const int BRIDGE_SENS_A = 9;
+const int BRIDGE_SENS_B = 10;
+const int TRACK_SENS_1 = 11;
+const int LIMIT_SW = 12;
+
+//* Stepper Motor:
+
+const int EN_PIN = 14;
+const int DIR = 15;
+const int STEP = 16;
+
+//! GLOBAL VARIABLES: --------------------------------------------------------------------------
+
+const int stepsPerRevolution = 3200; // It's actually 3200 but the library is broken...
+int stepCount = 0;
+
+//* OBJECTS: -----------------------------------------------------------------------------------
+
+
+//* FUNCTION PROTOTYPES: ----------------------------------------------------------------------
+
+void home(), test(int dir);
+
+//* MAIN CODE: ---------------------------------------------------------------------------------
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(EN_PIN, OUTPUT);
+    pinMode(DIR, OUTPUT);
+    pinMode(STEP, OUTPUT);
+
+    pinMode(BRIDGE_SENS_A, INPUT);
+    pinMode(BRIDGE_SENS_B, INPUT);
+    pinMode(TRACK_SENS_1, INPUT);
+    pinMode(LIMIT_SW, INPUT);
+
+    digitalWrite(EN_PIN, LOW);
+
 }
 
-void loop() {
-    for (int i = 0; i < 4; i++) {
-        for (int y = 0; y < 20; y++) {
-            lcd.setCursor(y, i);
-            lcd.print("X");
-            delay(500);
-            lcd.clear();
+void loop()
+{
+    if (Serial.available() > 0) {
+        char incomingByte = Serial.read();
+        Serial.println(incomingByte);
+        if (incomingByte == 'A' || incomingByte == 'B') {
+            test(incomingByte);
+        }
+        else
+        if (incomingByte == 'H') {
+            home();
         }
     }
-} */
-
-#include <Stepper.h>
-
-const int stepsPerRevolution = 200; // Change this to match your motor's steps per revolution
-const int dirPin = 2;
-const int stepPin = 3;
-
-Stepper myStepper(stepsPerRevolution, stepPin, dirPin);
-
-void setup() {
-  myStepper.setSpeed(500); // Adjust the speed as needed
 }
 
-void loop() {
-  // Turn the motor left
-  myStepper.step(stepsPerRevolution);
-  delay(1000);
+void home()
+{
+    digitalWrite(EN_PIN, HIGH);
+    digitalWrite(DIR, HIGH);
+    while (digitalRead(LIMIT_SW) == HIGH) {
+        digitalWrite(STEP, HIGH);
+        delayMicroseconds(1000);
+        digitalWrite(STEP, LOW);
+        delayMicroseconds(1000);
+    }
+    digitalWrite(STEP, LOW);
+    digitalWrite(EN_PIN, LOW);
+    stepCount = 0;
+}
+
+void test(int dir)
+{
+    if (dir == 'A') {
+        digitalWrite(DIR, HIGH);
+    }
+    else if (dir == 'B') {
+        digitalWrite(DIR, LOW);
+    }
+    digitalWrite(EN_PIN, HIGH);
+    for (int i = 0; i < stepsPerRevolution/4; i++) {
+        digitalWrite(STEP, HIGH);
+        delayMicroseconds(500);
+        digitalWrite(STEP, LOW);
+        delayMicroseconds(500);
+    }
+    digitalWrite(EN_PIN, LOW);
 }
